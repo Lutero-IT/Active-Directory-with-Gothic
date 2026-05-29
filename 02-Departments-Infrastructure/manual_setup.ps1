4180f0f4c43c5567f4f7c74ae0a9fa50d127702b
Lutero-IT <lutero.it@gmail.com>
2026-05-29 09:37:32 +0200

    feat: wprowadzenie dynamicznej walidacji grupy AD za pomocą pętli while

--- 02-Departments-Infrastructure/manual_setup.ps1
+++ 02-Departments-Infrastructure/manual_setup.ps1
@@ -1,3 +1,21 @@
+$GroupName = ""
+$GroupsList = Get-ADGroup -SearchBase "OU=User Groups,OU=Groups,OU=Camp,DC=oldcamp,DC=gothic,DC=inc" -Filter * | Select-Object -ExpandProperty Name
+
+while ( -not ($GroupsList -contains $GroupName)) {
+    echo "Please choose a group for which you want to create a Folder."
+    echo "The available groups are:"
+    echo $GroupsList
+    
+    $GroupName = Read-Host -Prompt "Type a group name"
+    echo "### Checking if $GroupName is a valid group name ###"
+
+    if ( -not ($GroupsList -contains $GroupName)) {
+    echo "Provided Group doesn't exist."
+    }
+}
+
+echo "Provided group is on a list. Creating Folder for a group..."
+
 ### Tworzenie Folderów ###
 # The location under which I executed the commands was: 
 # [OldCampServer]: PS C:\
